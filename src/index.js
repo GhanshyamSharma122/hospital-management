acbb4823420c1dd6147739cfad9427cf4fc94b99
shyam poudel <poudelshyam844@gmail.com>
2025-07-05 12:38:36 +0530

    feat: set up Express server with middleware and MongoDB connection
    - Added Express, CORS, cookie-parser, and dotenv as dependencies in package.json
    - Created app.js to initialize Express app with CORS and body parsing middleware
    - Added constants.js to define database name
    - Implemented MongoDB connection logic in index.js
    - Created db/index.js for database connection handling

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config({path:'./.env'})
+import connectDB from './db/index.js'
+import { app } from './app.js'
+
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log('error starting the server',error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at port: ${process.env.PORT}`);
+    })
+}).catch((err)=>{
+    console.log("mongo db connection failed",err)
+})
